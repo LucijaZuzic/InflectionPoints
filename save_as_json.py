4ca4c134d915a91ac30a946d47b50cd1cb42ccd6
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-02-18 10:35:00 +0100

    Added no data

--- save_as_json.py
+++ save_as_json.py
@@ -6,6 +6,8 @@ all_subdirs = os.listdir()
  
 dict_all_all_data = {"vehicles": []}
 
+dict_all_all_no_data = {"vehicles": []}
+
 for subdir_name in all_subdirs:
         
     if not os.path.isdir(subdir_name) or "Vehicle" not in subdir_name:
@@ -18,7 +20,11 @@ for subdir_name in all_subdirs:
 
     dict_all_all_data["vehicles"].append({"vehicle": vehicle_no, "rides": []})
 
+    dict_all_all_no_data["vehicles"].append({"vehicle": vehicle_no, "rides": []})
+
     dict_all_subdir_data = {"vehicles": [{"vehicle": vehicle_no, "rides": []}]}
+
+    dict_all_subdir_no_data = {"vehicles": [{"vehicle": vehicle_no, "rides": []}]}
     
     all_files = os.listdir(subdir_name + "/cleaned_csv/") 
 
@@ -39,6 +45,12 @@ for subdir_name in all_subdirs:
         dict_all_subdir_data["vehicles"][-1]["rides"].append({"ride": ride_no, "data": []})
 
         dict_all_data = {"vehicles": [{"vehicle": vehicle_no, "rides": [{"ride": ride_no, "data": []}]}]} 
+
+        dict_all_all_no_data["vehicles"][-1]["rides"].append({"ride": ride_no})
+
+        dict_all_subdir_no_data["vehicles"][-1]["rides"].append({"ride": ride_no})
+
+        dict_all_no_data = {"vehicles": [{"vehicle": vehicle_no, "rides": [{"ride": ride_no}]}]} 
 
         longitudes = list(file_with_ride["fields_longitude"])  
         latitudes = list(file_with_ride["fields_latitude"]) 
@@ -62,9 +74,23 @@ for subdir_name in all_subdirs:
         with open(new_dir + "/" + str(vehicle_no) + "_" + str(ride_no) + ".json", "w") as outfile: 
             json.dump(dict_all_data, outfile)
 
+        new_dir_no_data = "cleaned_json_no_data/" + str(vehicle_no) + "/" + str(ride_no)
+
+        if not os.path.isdir(new_dir_no_data):
+            os.makedirs(new_dir_no_data)
+
+        with open(new_dir_no_data + "/" + str(vehicle_no) + "_" + str(ride_no) + "_no_data.json", "w") as outfile: 
+            json.dump(dict_all_no_data, outfile)
+
     if is_ok:
         with open("cleaned_json/" + str(vehicle_no) + "/all_data_" + str(vehicle_no) + ".json", "w") as outfile: 
             json.dump(dict_all_subdir_data, outfile)
 
+        with open("cleaned_json_no_data/" + str(vehicle_no) + "/all_data_" + str(vehicle_no) + "_no_data.json", "w") as outfile: 
+            json.dump(dict_all_subdir_no_data, outfile)
+
 with open("all_data.json", "w") as outfile: 
     json.dump(dict_all_all_data, outfile)
+
+with open("all_no_data.json", "w") as outfile: 
+    json.dump(dict_all_all_no_data, outfile)
