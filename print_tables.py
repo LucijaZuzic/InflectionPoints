83c6b9f6219b77693c5da82a0c01ef3816d7a6fd
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-06-12 09:44:05 +0200

    Change format

--- print_tables.py
+++ print_tables.py
@@ -1,11 +1,117 @@
 import numpy as np
 import pandas as pd
+import os
+
+def process_ws(ws):
+
+    class_file = dict()
+    for algo in ["DBSCAN", "KMeans"]:
+        class_file[algo] = dict()   
+        for num_clus in list_clus:
+            class_file[algo][num_clus] = dict()
+            new_name = "clustered/" + algo + "/" + str(num_clus) + "/all_percent_1/clustered_" + algo + "_" + str(num_clus) + "_marker_all_percent_1_" + str(ws) + ".csv"
+            file_new_name = pd.read_csv(new_name, index_col = False)
+            for ix in range(len(file_new_name["vehicle"])):
+                nn = file_new_name["vehicle"][ix].split("_")[1] + "_" +  str(file_new_name["ride"][ix]).split("_")[1]
+                class_file[algo][num_clus][nn] = file_new_name["cluster"][ix]
+  
+    file_distance_json = open("count_me/all_percent_1/marker_all_percent_1_" + str(ws) + ".json", "r")
+
+    distance_json = eval(file_distance_json.readlines()[0])
+
+    dict_actual = dict()
+
+    dict_actual_5 = dict()
+    dict_actual_20 = dict()
+    dict_how_many_classes = dict()
+    for algo in ["DBSCAN", "KMeans"]:
+        dict_actual_5[algo] = dict()
+        dict_actual_20[algo] = dict()
+        dict_how_many_classes[algo] = dict()
+        for num_clus in list_clus:
+            dict_actual_5[algo][num_clus] = dict()
+            dict_actual_20[algo][num_clus] = dict()
+            dict_how_many_classes[algo][num_clus] = set()
+
+    for ix in range(len(distance_json["compare_to"])):
+        merge_reference = str(distance_json["compare_to"][ix]["vehicle"]) + "_" + str(distance_json["compare_to"][ix]["ride"])
+        dict_actual[merge_reference] = dict() 
+        for algo in ["DBSCAN", "KMeans"]:
+            for num_clus in list_clus:
+                dict_actual_5[algo][num_clus][merge_reference] = 0
+                dict_actual_20[algo][num_clus][merge_reference] = 0
+        for other_ix in range(len(distance_json["compare_to"][ix]["similar"])):
+            other_reference = str(distance_json["compare_to"][ix]["similar"][other_ix]["compare_vehicle"]) + "_" + str(distance_json["compare_to"][ix]["similar"][other_ix]["compare_ride"])
+            dist_ref = distance_json["compare_to"][ix]["similar"][other_ix]["distance"]
+            ord_ref = distance_json["compare_to"][ix]["similar"][other_ix]["order"]
+            dict_actual[merge_reference][other_reference] = (ord_ref, dist_ref)
+            for algo in ["DBSCAN", "KMeans"]:
+                for num_clus in list_clus:
+                    cls_me = class_file[algo][num_clus][merge_reference]
+                    cls_other = class_file[algo][num_clus][other_reference]
+                    dict_how_many_classes[algo][num_clus].add(cls_me)
+                    dict_how_many_classes[algo][num_clus].add(cls_other)
+                    if cls_me == cls_other:
+                        dict_actual_20[algo][num_clus][merge_reference] += 1
+                        if ord_ref < 5 * 114 + 1:
+                            dict_actual_5[algo][num_clus][merge_reference] += 1
+    
+    usable_ref = set()
+    banned = ["oldsz6oBFprLHMCOZ8RrY5qC4NqyWN2", "uYtOqsFbKhhEjc3zFe1AlHDaGEz2", "wzn4aVKA1Ja8D7ifTq1NzOCaWex1", "9Y00mt5TXdb2jZnDHANZQJjFzK43"] 
+    for user_id_file in os.listdir("marked/" + str(ws)):
+        user_id = user_id_file.replace("_" + str(ws) + ".csv", "")
+        if user_id in banned: 
+            continue 
+        file_user_id = pd.read_csv("marked/" + str(ws) + "/" + user_id_file, sep = ";", index_col = False)
+        for ix in range(len(file_user_id["vehicle"])):
+            merge_reference = str(file_user_id["vehicle"][ix]) + "_" + str(file_user_id["ride"][ix])
+            usable_ref.add(merge_reference)
+        break
+
+    dict_user = dict()
+    dict_user_clus = dict()
+    for algo in ["DBSCAN", "KMeans"]:
+        dict_user_clus[algo] = dict()
+        for num_clus in list_clus:
+            dict_user_clus[algo][num_clus] = dict()
+
+    for user_id_file in os.listdir("marked/" + str(ws)):
+        user_id = user_id_file.replace("_" + str(ws) + ".csv", "")
+        if user_id in banned: 
+            continue  
+        dict_user[user_id] = dict()
+        for algo in ["DBSCAN", "KMeans"]:
+            for num_clus in list_clus:
+                dict_user_clus[algo][num_clus][user_id] = dict()
+        file_user_id = pd.read_csv("marked/" + str(ws) + "/" + user_id_file, sep = ";", index_col = False)
+        for ix in range(len(file_user_id["vehicle"])): 
+            merge_reference = str(file_user_id["vehicle"][ix]) + "_" + str(file_user_id["ride"][ix])
+            dict_user[user_id][merge_reference] = file_user_id["chosen"][ix].split(":")
+            dict_user[user_id][merge_reference] = [oi.split("_")[0] + "_" + oi.split("_")[1] for oi in dict_user[user_id][merge_reference]]
+            for algo in ["DBSCAN", "KMeans"]:
+                for num_clus in list_clus:
+                    dict_user_clus[algo][num_clus][user_id][merge_reference] = 0
+                    for other_reference in dict_user[user_id][merge_reference]:
+                        cls_me = class_file[algo][num_clus][merge_reference]
+                        cls_other = class_file[algo][num_clus][other_reference]
+                        dict_how_many_classes[algo][num_clus].add(cls_me)
+                        dict_how_many_classes[algo][num_clus].add(cls_other)
+                        if cls_me == cls_other:
+                            dict_user_clus[algo][num_clus][user_id][merge_reference] += 1 
+  
+    return dict_user_clus, dict_actual_20, dict_how_many_classes
 
 list_clus_short = list(range(2, 21))
 
 list_clus = list(range(2, 21))
 for r in list(range(100, 121)):
     list_clus.append(r)
+
+algo_list_short = ["KMeans"]
+algo_list = ["KMeans", "DBSCAN"]
+
+dict_user_clus1, dict_actual_201, dict_how_many_classes1 = process_ws(10)
+dict_user_clus2, dict_actual_202, dict_how_many_classes2 = process_ws(20)
 
 did_choose_set_dict = dict()
 euclid_yes_no_dict = dict()
@@ -41,24 +147,82 @@ for ix in range(len(dict_how_many_classes_merged["ws"])):
     class_marker, size = dict_how_many_classes_merged["class"][ix], dict_how_many_classes_merged["size"][ix]
     dict_how_many_classes[algo][num_clus][ws][class_marker] = size
 
-print("Parameter & KMeans 10 & KMeans 20 & DBSCAN 10 size & DBSCAN 10 & DBSCAN 20 size & DBSCAN 20 \\\\ \\hline")
+use_compare = True
+use_numclus = False
+
+printer = "Parameter"
+for algo in algo_list_short:
+    if algo != "KMeans" and use_numclus:
+        printer += " & Clusters"
+    if use_compare:
+            printer += " & " + algo + " 10 Anomaly & " + algo + " 10 Cluster & " + algo + " 20 Anomaly & " + algo + " 20 Cluster"
+    else:
+            printer += " & " + algo + " 10 Anomaly & " + algo + " 20 Anomaly"
+printer += " \\\\ \\hline\n"
 for num_clus in list_clus_short:
-    printer = ""
-    for algo in ["KMeans", "DBSCAN"]:
+    for algo in algo_list_short:
         v3 = np.round(did_choose_set_dict[algo][num_clus][10], 2)
         v6 = np.round(did_choose_set_dict[algo][num_clus][20], 2)
-        if "DBSCAN" == algo:
-            printer += " & " + str(len(dict_how_many_classes[algo][num_clus][10])) + " & " + str(v3) + " & " + str(len(dict_how_many_classes[algo][num_clus][20])) + " & " + str(v6)
+        avg_user = dict()
+        avg_avg_user = dict()
+        for user_id in dict_user_clus1[algo][num_clus]:
+            avg_user[user_id] = []
+            for ref in dict_user_clus1[algo][num_clus][user_id]:
+                if min(dict_actual_201[algo][num_clus][ref], 5) != 0:
+                    avg_user[user_id].append(dict_user_clus1[algo][num_clus][user_id][ref] / min(dict_actual_201[algo][num_clus][ref], 5))
+            avg_avg_user[user_id] = np.average(avg_user[user_id])
+        avg_avg_avg_user = list(avg_avg_user.values())
+        v31 = np.round(np.average(avg_avg_avg_user) * 100, 2)
+        avg_2user = dict()
+        avg_2avg_2user = dict()
+        for user_id in dict_user_clus2[algo][num_clus]:
+            avg_2user[user_id] = []
+            for ref in dict_user_clus2[algo][num_clus][user_id]:
+                if min(dict_actual_202[algo][num_clus][ref], 5) != 0:
+                    avg_2user[user_id].append(dict_user_clus2[algo][num_clus][user_id][ref] / min(dict_actual_202[algo][num_clus][ref], 5))
+            avg_2avg_2user[user_id] = np.average(avg_2user[user_id])
+        avg_2avg_2avg_2user = list(avg_2avg_2user.values())
+        v61 = np.round(np.average(avg_2avg_2avg_2user) * 100, 2)
+        printer += str(num_clus)
+        if algo != "KMeans" and use_numclus:
+            printer += " & " + str(len(dict_how_many_classes[algo][num_clus][ws]))
+        if use_compare:
+            printer += " & " + str(v3) + " & " + str(v31) + " & " + str(v6) + " & " + str(v61)
         else:                
-            printer += str(num_clus) + " & " + str(v3) + " & " + str(v6)
-    print(printer, "\\\\ \\hline")
-    
-for algo in ["KMeans", "DBSCAN"]:
+            printer += " & " + str(v3) + " & " + str(v6)
+    printer += " \\\\ \\hline\n"
+print(printer)
+
+for algo in algo_list_short:
     for ws in [10, 20]:
-        print(algo + "_" + str(ws)) 
-        print("Parameter & both_euclid & both_ord & none_euclid & none_ord & one_one_euclid & one_one_ord \\\\ \\hline")
+        print(algo + " " + str(ws)) 
+        print("Parameter & both euclid & both ord & none euclid & none ord & one one euclid & one one ord \\\\ \\hline")
         printer = ""
         for num_clus in list_clus_short:
             both_euclid, both_ord, none_euclid, none_ord, one_one_euclid, one_one_ord = euclid_yes_no_dict[algo][num_clus][ws]
             printer += str(num_clus) + " & " + str(np.round(np.average(both_euclid), 4)) + " & " + str(np.round(np.average(both_ord), 2)) + " & " + str(np.round(np.average(none_euclid), 4)) + " & " + str(np.round(np.average(none_ord), 2)) + " & " + str(np.round(np.average(one_one_euclid), 4)) + " & " + str(np.round(np.average(one_one_ord), 2)) + " \\\\ \\hline\n"
         print(printer)
+        
+file_distance_json = open("count_me/all_percent_1/marker_all_percent_1_" + str(ws) + ".json", "r")
+
+distance_json = eval(file_distance_json.readlines()[0])
+
+dict_actual = dict()
+
+for ix in range(len(distance_json["compare_to"])):
+    merge_reference = str(distance_json["compare_to"][ix]["vehicle"]) + "_" + str(distance_json["compare_to"][ix]["ride"])
+    dict_actual[merge_reference] = dict() 
+    for other_ix in range(len(distance_json["compare_to"][ix]["similar"])):
+        other_reference = str(distance_json["compare_to"][ix]["similar"][other_ix]["compare_vehicle"]) + "_" + str(distance_json["compare_to"][ix]["similar"][other_ix]["compare_ride"])
+        dist_ref = distance_json["compare_to"][ix]["similar"][other_ix]["distance"]
+        ord_ref = distance_json["compare_to"][ix]["similar"][other_ix]["order"]
+        dict_actual[merge_reference][other_reference] = (ord_ref, dist_ref)
+
+all_euclid = []
+all_ord = []
+for r1 in dict_actual:
+    for r2 in dict_actual[r1]:
+        ord, euclid = dict_actual[r1][r2]
+        all_euclid.append(euclid)
+        all_ord.append(ord)
+print(np.round(np.average(all_euclid), 4), np.round(np.average(all_ord), 2))
--- print_tables_avg_yes_no.py
+++ print_tables_avg_yes_no.py
@@ -6,10 +6,13 @@ list_clus_short = list(range(2, 21))
 list_clus = list(range(2, 21))
 for r in list(range(100, 121)):
     list_clus.append(r)
+
+algo_list_short = ["KMeans"]
+algo_list = ["KMeans", "DBSCAN"]
     
 avg_yes_no_dict = dict()
 dict_how_many_classes = dict()
-for algo in ["KMeans", "DBSCAN"]:
+for algo in algo_list:
     avg_yes_no_dict[algo] = dict()
     dict_how_many_classes[algo] = dict()
     for num_clus in list_clus:
@@ -33,27 +36,59 @@ for ix in range(len(dict_how_many_classes_merged["ws"])):
 changed = dict()
 changed[10] = set()
 changed[20] = set()
-for algo in ["KMeans", "DBSCAN"]:
+for algo in algo_list:
     for ws in [10, 20]:
-        for num_clus in list_clus:
+        for num_clus in list_clus_short:
             #print(algo, ws, num_clus)
             ix = 0
             for seq in avg_yes_no_dict[algo][num_clus][ws]:
                 if avg_yes_no_dict[algo][num_clus][ws][seq][2] >= 0.1:
                     ix += 1
-                    if abs(avg_yes_no_dict[algo][num_clus][ws][seq][0] - avg_yes_no_dict[algo][num_clus][ws][seq][1]) >= 0.01:
+                    if abs(avg_yes_no_dict[algo][num_clus][ws][seq][0] - avg_yes_no_dict[algo][num_clus][ws][seq][1]) >= 0.1:
                         changed[ws].add(seq)
                         #print(algo, ws, num_clus, seq, np.round(avg_yes_no_dict[algo][num_clus][ws][seq][0] * 100, 2), np.round(avg_yes_no_dict[algo][num_clus][ws][seq][1] * 100, 2), avg_yes_no_dict[algo][num_clus][ws][seq][2] * 100, 2)
+
+use_numclus = False
 print(changed)
+
 for ws in changed:
     for seq in changed[ws]:
         v3 = np.round(avg_yes_no_dict["KMeans"][2][ws][seq][2] * 100, 2)
-        print(ws, seq, v3)
-        printer = ""
-        for num_clus in list_clus:
-            v1 = np.round(avg_yes_no_dict["KMeans"][num_clus][ws][seq][0] * 100, 2)
-            v2 = np.round(avg_yes_no_dict["KMeans"][num_clus][ws][seq][1] * 100, 2)
-            v4 = np.round(avg_yes_no_dict["DBSCAN"][num_clus][ws][seq][0] * 100, 2)
-            v5 = np.round(avg_yes_no_dict["DBSCAN"][num_clus][ws][seq][1] * 100, 2)
-            printer += str(num_clus) + " & " + str(v1) + " & " + str(v2) + " & " + str(v4) + " & " + str(v5) + " \\\\ \\hline\n"
-        print(printer)
+        #print(ws, seq, v3)
+        printer = "Parameter" 
+        for algo in algo_list_short:
+            if algo != "KMeans" and use_numclus:
+                printer += " & Clusters"
+            printer += " & " + algo + " yes & " + algo + " no"
+        printer += " \\\\ \\hline\n"
+        for num_clus in list_clus_short:
+            printer += str(num_clus)
+            for algo in algo_list_short:
+                v1 = np.round(avg_yes_no_dict[algo][num_clus][ws][seq][0] * 100, 2)
+                v2 = np.round(avg_yes_no_dict[algo][num_clus][ws][seq][1] * 100, 2)
+                if algo != "KMeans" and use_numclus:
+                    printer += " & " + str(len(dict_how_many_classes[algo][num_clus][ws]))
+                printer += " & " + str(v1) + " & " + str(v2)
+            printer += " \\\\ \\hline\n"
+        #print(printer)
+
+for algo in algo_list_short:
+    for seq in changed[10]:
+        if seq in changed[20]:
+            v3 = np.round(avg_yes_no_dict[algo][2][10][seq][2] * 100, 2)
+            v6 = np.round(avg_yes_no_dict[algo][2][20][seq][2] * 100, 2)
+            print(seq, v3, v6)
+            printer = "Parameter"
+            if algo != "KMeans" and use_numclus:
+                printer += " & Clusters"
+            printer += " & " + algo + " 10 yes & " + algo + " 10 no & " + algo + " 20 yes & " + algo + " 20 no \\\\ \\hline\n"
+            for num_clus in list_clus_short:
+                v1 = np.round(avg_yes_no_dict[algo][num_clus][10][seq][0] * 100, 2)
+                v2 = np.round(avg_yes_no_dict[algo][num_clus][10][seq][1] * 100, 2)
+                v4 = np.round(avg_yes_no_dict[algo][num_clus][20][seq][0] * 100, 2)
+                v5 = np.round(avg_yes_no_dict[algo][num_clus][20][seq][1] * 100, 2)
+                printer += str(num_clus)
+                if algo != "KMeans" and use_numclus:
+                    printer += " & " + str(len(dict_how_many_classes[algo][num_clus][ws]))
+                printer += " & " + str(v1) + " & " + str(v2) + " & " + str(v4) + " & " + str(v5) + " \\\\ \\hline\n"
+            print(printer)
