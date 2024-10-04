99571d680e1b82ab459d075942bdbc7fb575d458
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-10-04 09:28:25 +0200

    Update dist2.py

--- dist2.py
+++ dist2.py
@@ -1,24 +1,9 @@
-from utilities import * 
-import os
-from sklearn.cluster import DBSCAN
+from utilities import *
 import pandas as pd
+from sklearn.cluster import KMeans, DBSCAN
 from sklearn.manifold import TSNE
-
-def random_colors(num_colors):
-    colors_set = []
-    for x in range(num_colors):
-        string_color = "#"
-        while string_color == "#" or string_color in colors_set:
-            string_color = "#"
-            set_letters = "0123456789ABCDEF"
-            for y in range(6):
-                string_color += set_letters[np.random.randint(0, 16)]
-        colors_set.append(string_color)
-    return colors_set
-
-list_clus = list(range(2, 21))
-for r in list(range(100, 121)):
-    list_clus.append(r)
+from sklearn.neighbors import NearestNeighbors
+import kneed
 
 for subdir_name in os.listdir("marker_count"):
     
@@ -69,16 +54,61 @@ for subdir_name in os.listdir("marker_count"):
                 Xrow.append(file_csv[val][ix])
             X.append(Xrow)
         X = np.array(X)
+        print(np.shape(X))
+        
+        X_embedded = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=3).fit_transform(X)
+        
+        distances_np = np.linalg.norm(X[:, None] - X, axis = 2)
 
-        for num_clus in list_clus:
+        min_nbs = 400
 
-            if not os.path.isdir("clustered/DBSCAN/" + str(num_clus) + "/" + subdir_name):
-                
-                os.makedirs("clustered/DBSCAN/" + str(num_clus) + "/" + subdir_name)  
-            
-            new_name = "clustered/DBSCAN/" + str(num_clus) + "/" + subdir_name + "/clustered_DBSCAN_" + str(num_clus) + "_" + csv_file
-            new_name_png = new_name.replace("csv", "png")
-            
-            distances = np.sort(distances, axis=0)
-            distances = distances[:,1]
-            plt.plot(distances)
+        neighbors = NearestNeighbors(n_neighbors = min_nbs)
+        neighbors_fit = neighbors.fit(X)
+        distances, indices = neighbors_fit.kneighbors(X)
+
+        distances = np.sort(distances, axis=0)
+        distances = distances[:,1]
+        plt.plot([ix for ix in range(len(distances))], distances)
+        plt.show()
+        plt.close()
+
+        # calculate and show knee/elbow
+        kneedle = kneed.KneeLocator([ix for ix in range(len(distances))], distances, curve = "convex", direction = "increasing")
+        knee_point = kneedle.knee
+        knee_point_dist = distances[knee_point]
+        elbow_point = kneedle.elbow
+        elbow_point_dist = distances[knee_point]
+        print('Knee: ', knee_point, knee_point_dist) 
+        print('Elbow: ', elbow_point, elbow_point_dist)
+        kneedle.plot_knee()
+        plt.show()
+        plt.close()
+
+        if not os.path.isdir("clustered_new/DBSCAN/" + str(min_nbs) + "/" + subdir_name):
+            os.makedirs("clustered_new/DBSCAN/" + str(min_nbs) + "/" + subdir_name)
+
+        if not os.path.isdir("clustered_new/KMeans/" + str(min_nbs) + "/" + subdir_name):
+            os.makedirs("clustered_new/KMeans/" + str(min_nbs) + "/" + subdir_name)
+
+        new_name_dbscan  = "clustered_new/DBSCAN/" + str(min_nbs) + "/" + subdir_name + "/clustered_DBSCAN_" + str(min_nbs) + "_" + csv_file
+
+        dbscan = DBSCAN(min_samples = min_nbs, eps = elbow_point_dist).fit(X)
+        list_labels_dbscan = list(dbscan.labels_)
+        
+        new_data_csv_dbscan = {"vehicle": list(file_csv["vehicle"]), "ride": list(file_csv["ride"]), "cluster": list_labels_dbscan, 
+                        "TSNE_f1": [X_embedded[ix][0] for ix in range(len(list_labels_dbscan))], 
+                        "TSNE_f2": [X_embedded[ix][1] for ix in range(len(list_labels_dbscan))]}
+        df_new_data_csv_dbscan = pd.DataFrame(new_data_csv_dbscan)
+        df_new_data_csv_dbscan.to_csv(new_data_csv_dbscan, index = False)
+
+        print(len(list_labels_dbscan))
+        
+        kmeans = KMeans(n_clusters=len(list_labels_dbscan), random_state=0, n_init="auto").fit(X)
+        list_labels_kmeans = list(kmeans.labels_)
+        new_name_kmeans  = "clustered_new/KMeans/" + str(len(list_labels_dbscan)) + "/" + subdir_name + "/clustered_DBSCAN_" + str(num_clus) + "_" + csv_file
+
+        new_data_csv_kmeans = {"vehicle": list(file_csv["vehicle"]), "ride": list(file_csv["ride"]), "cluster": list_labels_kmeans, 
+                        "TSNE_f1": [X_embedded[ix][0] for ix in range(len(list_labels_kmeans))], 
+                        "TSNE_f2": [X_embedded[ix][1] for ix in range(len(list_labels_kmeans))]}
+        df_new_data_csv_kmeans = pd.DataFrame(new_data_csv_kmeans)
+        df_new_data_csv_kmeans.to_csv(new_name_kmeans, index = False)
